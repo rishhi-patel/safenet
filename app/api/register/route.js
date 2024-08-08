e38e77db09d332396d9d3b5b8972507d7590d5e5
Rishhi <98315242+rishhi-patel@users.noreply.github.com>
2024-08-08 12:51:29 -0400

    login config

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server"
-import db from "../../../models"
+import User from "../../../models/user"
 
 export async function POST(request) {
   const { email, password } = await request.json()
 
   try {
-    const newUser = await db.User.create({ email, password })
+    const newUser = await User.create({ email, password })
     return NextResponse.json(newUser, { status: 201 })
   } catch (error) {
     return NextResponse.json(
